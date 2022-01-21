856b55bd1872167acdb4a4e98db4938332231a2c
Mateusz Chodyra <mat.chodyra@gmail.com>
2022-01-21 13:57:18 +0100

    initial commit, eth custom token swap2

---
+++ src/components/App.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react';
+import Web3 from 'web3';
+import Token from '../abis/Token.json'
+import EthSwap from '../abis/EthSwap.json'
+import Navbar from './Navbar'
+import Main from './Main'
+import Sell from './Sell'
+import './App.css';
+
+class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0]})
+    console.log(this.state.account)
+
+    const ethBalance = await web3.eth.getBalance(this.state.account)
+    // this.setState({ ethBalance: ethBalance})
+    this.setState({ ethBalance })
+    console.log(this.state.ethBalance)
+ 
+    // Load token
+    const networkId = await web3.eth.net.getId()
+    const tokenData = Token.networks[networkId]
+    if(tokenData) { 
+      const token = new web3.eth.Contract(Token.abi, tokenData.address)
+      this.setState({ token })
+      let tokenBalance = await token.methods.balanceOf(this.state.account).call()
+      this.setState({ tokenBalance: tokenBalance.toString() })
+    } else {
+      window.alert('Token contract not deployed to detected network.')
+    }
+
+    //Load EthSwap
+    const ethSwapData = EthSwap.networks[networkId]
+    if(ethSwapData) { 
+      const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address)
+      this.setState({ ethSwap })
+      console.log(this.state.ethSwap)
+      const balance = this.state.token.methods.balanceOf(this.state.ethSwap.address).call()
+      console.log(balance.toString())
+    } else {
+      window.alert('EthSwap contract not deployed to detected network.')
+    }
+
+    const ethBalance1 = await web3.eth.getBalance(this.state.ethSwap.address)
+    // this.setState({ ethBalance: ethBalance})
+    this.setState({ ethBalance1 })
+    console.log(this.state.ethBalance1)
+
+    this.setState({ loading: false})
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if (window.web3) {
+      window.web3 = Web3(window.web3.currentProvider)
+    } else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  buyTokens = (etherAmount) => {
+    this.setState({ loading: true })
+    this.state.ethSwap.methods.buyTokens().send({ value: etherAmount, from: this.state.account }).on('transactionHash', (hash) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  sellTokens = (tokenAmount) => {
+    this.setState({ loading: true })
+    this.state.token.methods.approve(this.state.ethSwap.address, tokenAmount).send( { from: this.state.account } )
+    this.state.ethSwap.methods.sellTokens(tokenAmount).send( { from: this.state.account } ).on('transactionHash', (hash) => {
+      this.setState({ loading: false })
+    })
+  }
+  
+
+  constructor(props) {
+    super(props)
+    this.state = { 
+      account: '',
+      token: {},
+      ethSwap: {},
+      ethBalance: '0',
+      tokenBalance: '0',
+      loading: true
+    }
+  }
+
+  render() {
+    let buyForm
+    let sellForm
+    if(this.state.loading) {
+      buyForm = <p id="loader" className="text-center">Loading...</p>
+      sellForm = <p id="loader" className="text-center">¯\_(ツ)_/¯</p>
+    } else {
+      buyForm = <Main 
+        ethBalance={this.state.ethBalance} 
+        tokenBalance={this.state.tokenBalance}
+        buyTokens={this.buyTokens}
+      />
+      sellForm = <Sell 
+        ethBalance={this.state.ethBalance} 
+        tokenBalance={this.state.tokenBalance}
+        sellTokens={this.sellTokens}
+      />
+    }
+    return (
+      <div>
+        <Navbar account={this.state.account}/>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
+              <div className="content mr-auto ml-auto">
+                
+                {buyForm}
+              </div>
+              <div className="content mr-auto ml-auto">
+                
+                {sellForm}
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
